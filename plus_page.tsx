c004dbdf9c1c9921a63889bb674fa14b8902a338
ProjectResolutionsOffice <autocoding.pro@gmail.com>
2025-09-17 13:57:50 +0900

    Update plus_page.tsx

--- plus_page.tsx
+++ plus_page.tsx
@@ -504,13 +504,11 @@ const App = () => {
         </div>
       </div>
       <footer className="page-footer">
-        <div className="adfit-placeholder">
-            <ins className="kakao_ad_area" style={{ display: 'none' }}
+          <ins className="kakao_ad_area" 
                 data-ad-unit = "DAN-UYvLxRjSAWSkaXPW"
                 data-ad-width = "728"
                 data-ad-height = "90"></ins>
             <script type="text/javascript" src="//t1.daumcdn.net/kas/static/ba.min.js" async></script>
-        </div>
         <div className="footer-info">
           <div className="footer-text">
             <p>
@@ -544,4 +542,5 @@ if(container) {
     const root = createRoot(container);
     root.render(<App />);
 }
+
 
